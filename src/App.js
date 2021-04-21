3879ea635d38dbeb836adda40e8f1dd648c8e2e5
Ziad Hegazi <ziadhegazi@hotmail.com>
2021-04-21 02:17:49 +0200

    created a form + style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import logo from "./assets/ZH style 2 WIP3pdf.jpg";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className = "container">
+      <img src={logo} alt="Logo" className = "logo"></img>
+      <input type="email" placeholder="Email"></input>
+      <input type="password" placeholder="Password" ></input>
+      <button type = "submit" className="btn">Submit</button>
+    </form>
   );
 }
 
